5d72cefdcf148e9d9888325b0a680ccca635701e
zander007 <thuozander@gmail.com>
2020-12-03 22:57:08 +0300

    worked on header component

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB6HP6QnEIPws5y0lVxnuvxwvgJKyopSxk",
+    authDomain: "facebook-clone-1087d.firebaseapp.com",
+    projectId: "facebook-clone-1087d",
+    storageBucket: "facebook-clone-1087d.appspot.com",
+    messagingSenderId: "63343723069",
+    appId: "1:63343723069:web:ceb64a325a7202f7aee054",
+    measurementId: "G-9DX6JVS4SZ"
+  };
---
+++ src/pages/Homepage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../component/Header'
+
+const Homepage = () => {
+    return (
+        <div className="app">
+            <Header />
+            <div className="app__body">
+            
+            </div>
+        </div>
+    )
+};
+
+export default Homepage;
