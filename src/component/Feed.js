bd9a7ac00b7efedd0a5855db239c10c7ad43a028
zander007 <thuozander@gmail.com>
2020-12-04 03:57:12 +0300

    worked on the message sender component

--- src/component/Feed.js
+++ src/component/Feed.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../styles/Feed.component.css';
+import MessageSender from './MessageSender';
 import StoryReel from './StoryReel';
 
 const Feed = () => {
     return (
         <div className="feed">
             <StoryReel />
+            <MessageSender />
         </div>
     )
 }
