c94bed72adf4ac57d1e0169193d2ff1739f67c69
zander007 <thuozander@gmail.com>
2020-12-06 01:41:40 +0300

    worked on post component

--- src/component/Feed.js
+++ src/component/Feed.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/Feed.component.css';
 import MessageSender from './MessageSender';
+import Post from './Post';
 import StoryReel from './StoryReel';
 
 const Feed = () => {
@@ -8,6 +9,20 @@ const Feed = () => {
         <div className="feed">
             <StoryReel />
             <MessageSender />
+            <Post 
+                profilePic="https://avatars0.githubusercontent.com/u/21181649?s=460&u=9f26d8140b9f770bd9fed1f8fdc1338ae05fa713&v=4"
+                message='Wow it works'
+                timestamp='thisis a time stamp'
+                username='zander'
+                image='https://avatars0.githubusercontent.com/u/21181649?s=460&u=9f26d8140b9f770bd9fed1f8fdc1338ae05fa713&v=4'
+            />
+            <Post 
+                profilePic="https://avatars0.githubusercontent.com/u/21181649?s=460&u=9f26d8140b9f770bd9fed1f8fdc1338ae05fa713&v=4"
+                message='Wow it works'
+                timestamp='thisis a time stamp'
+                username='zander'
+                image='https://avatars0.githubusercontent.com/u/21181649?s=460&u=9f26d8140b9f770bd9fed1f8fdc1338ae05fa713&v=4'
+            />
         </div>
     )
 }
